0d0f0116796914d3e0c28461243790713d3196d3
Kris Maini-Thorsen <krismainithorsen@kriss-macbook-pro-2.home>
2021-04-08 12:02:24 +0200

    testing cms features: new content type

---
+++ components/global/Slider.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vue-slider id="slider-input" v-model="value" />
+  </div>
+</template>
+
+<script>
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+
+export default {
+  name: "Slider",
+  data: () => ({
+    value: 50
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
